9e6fc5e96273f87c6b35ca4ac0b13067f38e9c0c
Phillip Coitchev <p.coitchev@devrealm.bg>
2024-07-29 16:05:58 +0300

    Add link to author

---
+++ src/Components/Author.tsx
@@ -0,0 +1,37 @@
+import { Button, Typography } from "@mui/material";
+
+
+function Author() {
+  return (
+    <Button
+      sx={{
+        p: 0,
+        position: "absolute",
+        right: 10,
+        bottom: 50
+      }}
+      onClick={() => {
+        const newWindow = window.open("https://www.facebook.com/milenbubi", "_blank", "noopener,noreferrer");
+
+        if (newWindow) {
+          newWindow.opener = null;
+        }
+      }}
+    >
+      <Typography
+        sx={{
+          color: "lightblue",
+          fontWeight: 800,
+          fontSize: 15,
+          fontStyle: "italic"
+        }}
+      >
+        {"АВТОР"}
+      </Typography>
+    </Button>
+  );
+}
+
+
+
+export default Author;
