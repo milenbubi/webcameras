6e5541a7602304f215587c8434e9cdca93c5cf54
Phillip Coitchev <p.coitchev@devrealm.bg>
2024-08-07 14:51:24 +0300

    Reorder Kelebia cams

--- src/BCCP/Kelebia.tsx
+++ src/BCCP/Kelebia.tsx
@@ -8,17 +8,7 @@ import FSButton from "../Components/FSButton";
 function Kelebia() {
   return (  // Келебия
     <Stack gap={5} pt={3} width="100%" alignItems="center">
-
-
       <Centered gap={5} maxHeight="100%" width="100%" flexWrap="wrap">
-        <Centered id="ksrb" width="100%" maxHeight="100%" position="relative" sx={{ maxWidth: 700 }}>
-          <iframe
-            src="https://live.uzivokamere.com/kelebija1"
-            style={{ border: "none", width: "100%", maxHeight: "100%", aspectRatio: "16/9" }}
-          />
-          <Title value="Келебия - към Сърбия" />
-          <FSButton fsElementId="ksrb" />
-        </Centered>
 
 
         <Centered id="khun" width="100%" maxHeight="100%" position="relative" sx={{ maxWidth: 700 }}>
@@ -29,9 +19,19 @@ function Kelebia() {
           <Title value="Келебия - към Унгария" />
           <FSButton fsElementId="khun" />
         </Centered>
+
+
+        <Centered id="ksrb" width="100%" maxHeight="100%" position="relative" sx={{ maxWidth: 700 }}>
+          <iframe
+            src="https://live.uzivokamere.com/kelebija1"
+            style={{ border: "none", width: "100%", maxHeight: "100%", aspectRatio: "16/9" }}
+          />
+          <Title value="Келебия - към Сърбия" />
+          <FSButton fsElementId="ksrb" />
+        </Centered>
+
+
       </Centered>
-
-
     </Stack>
   );
 }
