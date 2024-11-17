a571f1de205a63590478ead32ecb8b9fc73c88ce
Phillip Coitchev <p.coitchev@devrealm.bg>
2024-11-17 13:35:17 +0200

    Remove any search params on start

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import * as ReactDOM from "react-dom/client";
 import { CssBaseline, GlobalStyles } from "@mui/material";
-// import ".@/Styles1/Scrollbar.scss";
 import AppEntry from "./AppEntry";
 import globalStyles from "./Styles/StylesGlobal";
 
 
 
 function CasinoPortalApp() {
+  useEffect(() => {  // Remove any search params 
+    if (window.location.search) {
+      window.history.pushState({}, document.title, window.location.origin);
+    }
+  }, []);
+
+
   return (
     <Fragment>
       <CssBaseline />
