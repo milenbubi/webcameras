f63499ae1684af3049835e1125726bd83be2983a
Phillip Coitchev <p.coitchev@devrealm.bg>
2025-04-10 08:41:51 +0300

    Change activator text

--- src/Components/NoStreamTitle.tsx
+++ src/Components/NoStreamTitle.tsx
@@ -3,10 +3,10 @@ import { Stack, Typography } from "@mui/material";
 
 function NoStreamTitle() {
   return (
-    <Stack gap={3} sx={{ height: 1, justifyContent: "center", py: 3 }}>
+    <Stack gap={3} sx={{ height: 1, justifyContent: "center", py: 3, px: { xs: 0, sm: 12, md: 30 } }}>
 
       <Typography align="center" variant="h4" fontWeight={1000}>
-        {"За да се избегне нежелан интернет трафик, видео стриймът е деактивиран"}
+        {"С цел предотвратяване на нежелан интернет трафик, видео стрийминг услугите са временно деактивирани"}
       </Typography>
 
       <Typography align="center" variant="h5" fontWeight={800}>
