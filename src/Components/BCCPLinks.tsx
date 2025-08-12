119765a6cbb22acdb25eed34313a0a7f1c37985b
Phillip Coitchev <p.coitchev@devrealm.bg>
2025-08-12 12:31:13 +0300

    Add link to Serbian MUP

--- src/Components/BCCPLinks.tsx
+++ src/Components/BCCPLinks.tsx
@@ -1,15 +1,16 @@
-import { Stack, CardMedia, Button } from "@mui/material";
+import { CardMedia, Button } from "@mui/material";
+import Centered from "../Utils/Centered";
 
 
 
 function BCCPLinks() {
   return (
-    <Stack direction="row" justifyContent="center" gap={4} sx={{ width: 1 }}>
+    <Centered rowGap={2} columnGap={0} sx={{ width: 1, flexWrap: "wrap" }}>
 
 
       {/* Линк към BorderAlarm */}
       <Button
-        sx={{ p: 0 }}
+        sx={{ p: 0, mx: 2 }}
         onClick={() => {
           const newWindow = window.open("https://borderalarm.com/", "_blank", "noopener,noreferrer");
 
@@ -29,7 +30,7 @@ function BCCPLinks() {
 
       {/* Линк към UzivoKamere */}
       <Button
-        sx={{ p: 0 }}
+        sx={{ p: 0, mx: 2 }}
         onClick={() => {
           const newWindow = window.open("https://uzivokamere.com/granicni-prelazi/", "_blank", "noopener,noreferrer");
 
@@ -47,7 +48,28 @@ function BCCPLinks() {
       </Button>
 
 
-    </Stack>
+
+      {/* Линк към камери на МВР - Сърбия */}
+      <Button
+        sx={{ p: 0 }}
+        onClick={() => {
+          const newWindow = window.open("https://mup.gov.rs/wps/portal/sr/kamer%D0%B5#!", "_blank", "noopener,noreferrer");
+
+          if (newWindow) {
+            newWindow.opener = null;
+          }
+        }}
+      >
+        <CardMedia
+          component="img"
+          src="/shots/mup.png"
+          sx={{ width: { xs: 30, sm: 50 }, borderRadius: 1 }}
+          onError={e => { e.currentTarget.style.display = "none"; }}
+        />
+      </Button>
+
+
+    </Centered>
   );
 }
 
