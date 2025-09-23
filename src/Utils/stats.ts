34ded5291cced5353b13b376f7ae0f6abbafe719
Phillip Coitchev <p.coitchev@devrealm.bg>
2025-09-23 11:30:14 +0300

    Rename php folder

--- src/Utils/stats.ts
+++ src/Utils/stats.ts
@@ -5,13 +5,13 @@ import { useEffect } from "react";
 export function useStats(place: string) {
   useEffect(() => {
     if (!window.location.port) {  // Запис на ново посещение
-      fetch(`/p/visit.php?page=${place}`, { method: "GET" })
+      fetch(`/php/visit.php?page=${place}`, { method: "GET" })
         .then(res => res.json())
         // .then(data => console.log("Visit logged:", data))
         .catch(err => { /* console.error(err); */ });
     }
     else {  // Извикваме данните - само за DEV цели
-      fetch("https://chan180.net/p/count.php", { method: "GET" })
+      fetch("https://chan180.net/php/stats.php", { method: "GET" })
         .then(res => res.json())
         .then(data => console.log("Site stats:", data))
         .catch(err => console.error(err));
